01a38a2bc4b50036fda6198b582070173b2e6aaa
Sabiya Tabassum <50179634+sabiyatabassum@users.noreply.github.com>
2021-05-27 14:55:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
